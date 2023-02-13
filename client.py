0e7de987b87d6bda7e2fc705131cd95ff895b269
Aneesh Muppidi <aneeshmuppidi19@gmail.com>
2023-02-12 23:41:45 -0500

    Read from server

--- client.py
+++ client.py
@@ -13,7 +13,6 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-
 alias = input('Choose an alias >>> ')
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 59000))
@@ -23,10 +22,14 @@ def client_receive():
     while True:
         try:
             message = client.recv(1024).decode('utf-8')
+            if "you are now connected!" in message:
+                print("We have connected. \n")
             if message == "alias?":
                 client.send(alias.encode('utf-8'))
+            elif message == "The user you are trying to contact does not exist.":
+                print(bcolors.WARNING + message + bcolors.ENDC)
             else:
-                print(bcolors.OKBLUE + message + bcolors.ENDC)
+                print(bcolors.OKBLUE + message + bcolors.ENDC + '\n', end='')
         except:
             print('Error!')
             client.close()
@@ -34,9 +37,15 @@ def client_receive():
 
 
 def client_send():
+    firstMessage = True
     while True:
-        message = f'{alias}-> {input("")}'
-        client.send(message.encode('utf-8'))
+        inp = input("What would you like to say? To whom?\n")
+        if "->" not in inp:
+            print(bcolors.WARNING + "NEED TO SPECIFY USER. Correct usage: USER-> Message." + bcolors.ENDC)
+        else:    
+            message = f'{alias}-> {inp}'
+            client.send(message.encode('utf-8'))
+        
 
 
 receive_thread = threading.Thread(target=client_receive)

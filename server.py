22cdc42e54f3b64be5b106cfe60cbc7053dcc399
Aneesh Muppidi <aneeshmuppidi19@gmail.com>
2023-02-12 23:42:27 -0500

    Handles queued messages and login tracking

--- server.py
+++ server.py
@@ -1,5 +1,3 @@
-'Chat Room Connection - Client-To-Client'
-
 import threading
 import socket
 host = '127.0.0.1'
@@ -10,6 +8,9 @@ server.listen()
 clients = []
 aliases = []
 clientID = {}
+loginStatus = {}
+queuedMessages = {}
+
 
 def broadcast(message):
     for client in clients:
@@ -21,27 +22,46 @@ def sendToClient(alias, message):
     client = clientID[alias]
     client.send(message)
 
+
+def unPackMessage(client, data):
+
+    decodedM = data.decode('UTF-8').split("->")
+    sender = decodedM[0]
+    deliver = decodedM[1].strip()
+    deliver = deliver.encode('UTF-8')
+    message = (sender + " says -> " + decodedM[2]).encode('UTF-8')
+    return deliver, message
+
+
 def handle_client(client):
     while True:
         try:
-            message = client.recv(1024)
-            decodedM = message.decode('UTF-8').split("->")
-            sender = decodedM[0]
-            deliver = decodedM[1].strip()
-            deliver = deliver.encode('UTF-8')
-            #deliver = b'Reza'
-            if deliver in clientID:
-                sendToClient(deliver, (sender + " says -> " + decodedM[2]).encode('UTF-8'))
+            index = clients.index(client)
+            alias = aliases[index]
+            data = client.recv(1024)
+            deliver,message = unPackMessage(client, data)
+            if deliver not in loginStatus:
+                client.send(("The user you are trying to contact does not exist.").encode('UTF-8'))
+            elif not loginStatus[deliver]:
+                notlogin = deliver.decode('UTF-8') + " is not logged in."
+                notlogin = notlogin.encode('UTF-8')
+                client.send(notlogin)
+                if deliver not in queuedMessages.keys():
+                    queuedMessages[deliver] = []
+                queuedMessages[deliver].append(message)
+            elif (deliver in clientID) and (clientID[deliver] in clients):
+                sendToClient(deliver, message)
             else:
                 print(clientID.keys())
                 print(deliver + " does not exist")
-                broadcast(message)
+                broadcast(data)
 
         except:
             index = clients.index(client)
             clients.remove(client)
             client.close()
             alias = aliases[index]
+            loginStatus[alias] = False
             broadcast(f'{alias} has left the chat room!'.encode('utf-8'))
             aliases.remove(alias)
             break
@@ -60,9 +80,14 @@ def receive():
         aliases.append(alias)
         clients.append(client)
         clientID[alias] = client
+        loginStatus[alias] = True
         print(f'The alias of this client is {alias}'.encode('utf-8'))
         broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
-        client.send('you are now connected!'.encode('utf-8'))
+        client.send('you are now connected! \n'.encode('utf-8'))
+        if alias in queuedMessages.keys():
+            for message in queuedMessages[alias][:]:
+                client.send(message)
+                queuedMessages[alias].remove(message)
         thread = threading.Thread(target=handle_client, args=(client,))
         thread.start()
 

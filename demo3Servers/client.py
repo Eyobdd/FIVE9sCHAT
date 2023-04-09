a2c943e59f8dfb9fff1f66758f9f4ade353b7d54
Aneesh Muppidi <aneeshmuppidi19@gmail.com>
2023-04-09 03:14:38 -0400

    all servers connect to each other

---
+++ demo3Servers/client.py
@@ -0,0 +1,285 @@
+## TODO
+# 1. Write unit tests and say expected outputs -- Eyob
+# 2. Write setup documentation (Aneesh)
+# 4. Comment code (Eyob on socket), (Aneesh on GRPC)
+# 5. Clean up code
+# 6. Write decision documentation (Eyob on Socket), (Aneesh on GRPC), (Aneesh on intro and design strategy)
+# 9. Create Uniform Error Code System -- future
+# 10. Create decode abstraction.  -- if time
+
+
+# Import modules used
+import threading
+import socket
+import os
+import signal
+from message import Message
+
+# Constants used in text styling
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# Creates an encoded bitstring from a message using our wire protocol
+def encoded_message(message):
+    message = message.encode('utf-8')
+    header = f"{len(message) :< {HEADER_LENGTH}}".encode('utf-8')
+    return header+message
+
+# Defined header length throughout wire protocol
+HEADER_LENGTH = 10
+HOST = '10.250.52.110'
+# Create a socket and connect to the server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, 12340))
+
+
+# User initially starts unnamed and unauthenticated
+auth = False
+username = ''
+
+try:
+
+    ## Display all existing accounts
+
+    # Retrieves list from server of all existing accounts
+    header = client.recv(HEADER_LENGTH).decode('utf-8').strip()
+    data_length = int(header)
+    data = client.recv(data_length).decode('utf-8')
+
+    # Displays all accounts with their activity status
+    allAccounts = data.split("|")[1:]
+    print(bcolors.OKGREEN + "The server holds the following accounts." + bcolors.ENDC)
+    for account in allAccounts:
+        print(" -- > " + account)
+    
+    
+    # goBack loop lets user reselect if they want to login or create an account
+    goBack = True
+    while goBack:
+        
+        # Asks user if they would like to login or create an account
+        command = input("Type " + bcolors.BOLD + "C" + bcolors.ENDC + " to create an account. Type " + bcolors.BOLD + "L" + bcolors.ENDC + " to login in." )
+        
+        # Reprompt if user gives invalid answer
+        while command != "C" and command != "L":
+            print("Please select either C or L\n")
+            command = input("Type " + bcolors.BOLD + "C" + bcolors.ENDC + " to create an account. Type " + bcolors.BOLD + "L" + bcolors.ENDC + " to login in." )
+
+
+        # Authentication loop only broken if user goes back or is authenticated
+        while not auth:
+
+            # Asks for username and gives option to go back 
+            print("Usernames must be alphanumeric characters.\n(Type "+bcolors.BOLD+"!"+bcolors.ENDC+" to go back)")
+            username = input("Please enter a username:")
+
+            # If user wants to create an account and submits a real username
+            if command == "C" and username != "!":
+                
+                # Sends Create account request to server
+                request_message = f"CA:{username}"
+                request_message = encoded_message(request_message)
+                client.send(request_message)
+
+                # Awaits and receives server confirmation
+                header = client.recv(HEADER_LENGTH).decode('utf-8')
+                confirmation_length = int(header.strip())
+                confirmation = client.recv(confirmation_length).decode('utf-8')
+                
+                # Takes the confirmation message only
+                confirmation = confirmation.split(":",3)[3]
+                
+                # Checks if request is successful
+                if confirmation == "Successful-Account-Creation.":
+                    
+                    # User is authorized
+                    auth = True
+                    goBack = False
+                    
+                    # Print confirmation message
+                    print(bcolors.OKGREEN + confirmation + bcolors.ENDC)
+
+                else:
+
+                    # Print confirmation message
+                    print(bcolors.FAIL + confirmation + bcolors.ENDC)
+
+            # If user submits a real username and command type is L
+            elif username != "!":
+                
+                # Send Login request to server
+                request_message = f"L:{username}"
+                request_message = encoded_message(request_message)
+                client.send(request_message)
+
+                # Awaits and recieves confirmation from server
+                header = client.recv(HEADER_LENGTH).decode('utf-8').strip()
+                confirmation_length = int(header.strip())
+                confirmation = client.recv(confirmation_length).decode('utf-8')
+
+                # Retrieve confirmation message
+                confirmation = confirmation.split(":",3)[3]
+
+                # Checks if request is successful
+                if confirmation == "Login-Successful.":
+                    # User is authorized
+                    auth = True
+                    goBack = False
+                    
+                    # Print confirmation message
+                    print(bcolors.OKGREEN + confirmation + bcolors.ENDC)
+
+                else:
+                    
+                    # Print confirmation message
+                    print(bcolors.FAIL + confirmation + bcolors.ENDC)
+            
+            # If username is !, break out of auth loop and reprompt commands
+            else:
+                break            
+
+    # Thread that listens for communications from the server
+    def client_receive():
+        while True:
+            try:
+
+                # Receives data sent by server
+                data_header = client.recv(HEADER_LENGTH).decode('utf-8')
+                data_length = int(data_header.strip())
+                data = client.recv(data_length).decode('utf-8')
+
+                # Converts received data into a message object
+                data = Message.createMessageFromBuffer(data)
+
+                # Checks if message sender is SERVER
+                if (data.sender == "SERVER"):
+                    
+                    # If message object doesn't encode properly
+                    if data.data[0] == "<":
+                        continue
+
+                    # If SERVER is sending a list of active accounts
+                    if data.data[0:3] == "LA|":
+
+                        # Unpacks the sent list 
+                        allAccounts = data.data.split("|")[1:]
+                        
+                        # Prints list
+                        print(bcolors.OKGREEN + "The server holds the following accounts." + bcolors.ENDC)
+                        for account in allAccounts:
+                            print(" -- > " + account)
+
+                    # If SERVER Confirms account is successfully deleted
+                    elif data.data == "Account-Successfully-Deleted":
+
+                        # Print message to console
+                        print("Your account has been deleted. Your client connection will end. Bye, Bye")
+                        print(bcolors.BOLD + bcolors.FAIL + "EXITING CLIENT PROGRAM." + bcolors.ENDC)
+                        
+                        # Exits terminal and closes socket
+                        os.kill(os.getpid(), signal.SIGINT)
+                    
+                    # If SERVER cannot find and delete account 
+                    elif data.data == "Account-Does-Not-Exist":
+                        print(bcolors.FAIL + data.data + bcolors.ENDC)
+
+                    # If SERVER is sending a broadcast message
+                    else:
+                        print(bcolors.OKCYAN + "[" + data.sender + "] " + bcolors.ENDC + data.data)
+
+
+                # If the message is sent from another user
+                if data.sender != "SERVER":
+                    print(bcolors.OKCYAN + "[" + data.sender + "] " + bcolors.ENDC + data.data)
+
+            # Any general errors are caught here
+            except Exception as e:
+                print(bcolors.WARNING +'Error! '+ str(e) + bcolors.ENDC)
+                client.close()
+                break       
+
+
+    # Thread that sends authenticated communication to the server
+    def client_send():
+        
+        while True:
+
+            # Asks user for input
+            inp = input(bcolors.BOLD +"COMMANDS" + bcolors.ENDC + ": " + bcolors.BOLD + "\n" +"LA" + bcolors.ENDC + " - List accounts. "+ bcolors.BOLD + "\n" + "USERNAME-> MESSAGE" + bcolors.ENDC+ " - Send USERNAME MESSAGE." + "\n" + bcolors.BOLD + "DA" + bcolors.ENDC + " - Delete your account."+"\n" + bcolors.BOLD + "Q" + bcolors.ENDC + " - Quit client program."+"\n")
+            
+            # If user enters nothing
+            if inp == "":
+                print(bcolors.WARNING + "Empty input" + bcolors.ENDC)
+            
+            # User enters something
+            else:
+
+                # If input is not a message
+                if "->" not in inp:
+
+                    # Check if it List Accounts request
+                    if inp == "LA":
+
+                        # Sends List Accounts request
+                        message = f"LA:{username}"
+                        message = encoded_message(message)
+                        client.send(message)
+                        
+                    # Check if it is a Delete Account request
+                    elif inp == "DA":
+                        
+                        # Sends delete account request with authorized username
+                        message = f"DA:{username}"
+                        message = encoded_message(message)
+                        client.send(message)
+
+                    # Check if the user is trying to quit the program
+                    elif inp == "Q":
+                        
+                        # Prints exit message
+                        print(bcolors.BOLD + bcolors.FAIL + "EXITING CLIENT PROGRAM." + bcolors.ENDC)
+                        
+                        # Exits terminal and closes socket
+                        os.kill(os.getpid(), signal.SIGINT)
+                        break
+
+                    # User submitted invalid message syntax
+                    else:
+                        # Prints syntax warning message
+                        print(bcolors.WARNING + "NEED TO SPECIFY USER. Correct usage: USER-> Message." + bcolors.ENDC)
+                
+                # User submitted valid message syntax
+                else:
+                    # Breaks input into recipient and message based on "->"
+                    inputList = inp.split("->")
+                    recipient = inputList[0]
+                    message = inputList[1]
+
+                    # Creates a message object
+                    message = Message(recipient, username, message)
+                    message = message.encode()
+
+                    # Sends message to server
+                    client.send(message)
+
+
+            
+    # USER IS NOW AUTHENTICATED
+    # Open up thread to send and receive messages from server
+    receive_thread = threading.Thread(target=client_receive)
+    receive_thread.start()
+
+    send_thread = threading.Thread(target=client_send)
+    send_thread.start()
+
+# If user exits with ^C it is caught here
+except KeyboardInterrupt:
+    print(bcolors.BOLD + bcolors.FAIL + "EXITING CLIENT PROGRAM." + bcolors.ENDC)
---
+++ demo3Servers/server2.py
@@ -0,0 +1,380 @@
+import threading
+import socket
+
+from message import Message
+from command import Command
+import time
+# Constants
+HEADER_LENGTH = 10
+HOST = '10.250.209.143'
+PORT = 12341
+
+# Connect sockets to server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+# Information storage structures
+
+# Stores active socket
+clients = []
+
+# Stores active usernames
+usernames = [] 
+
+
+# Stores all active users {username:socket} pairs
+clientID = {}
+
+# Stores if a user is online or not all accounts {username:Boolean}
+loginStatus = {}
+
+# The server is always online so users cannot log into the server account
+loginStatus['SERVER'] = True
+
+# Stores all messages to offline users {username: Message()}
+queuedMessages = {}
+
+
+# Function sends encoded message to username
+def sendToClient(username, message):
+
+    # Creates Message() object
+    message = Message(username, "SERVER", message)
+
+    # Finds socket associated with user
+    client = clientID[username]
+
+    # Sends encoded message to that user
+    client.send(message.encode())
+
+# Function receives data from client
+def receiveData(client):
+    data = client.recv(HEADER_LENGTH).decode('utf-8')
+    data_length = int(data.strip())
+    data = client.recv(data_length).decode('utf-8')
+    return data
+
+# Protocol action handles all actions by the server according to our Protocol
+# Input is either a Message() or Command() object
+def protocol_action(obj):
+
+    # If the object is a Message() then we try to send the message
+    if isinstance(obj, Message):
+
+        # If recipient exists -> sends error to client if they do not
+        if obj.recipient not in loginStatus:
+            # doesNotExist = "The user you are trying to contact does not exist."
+            # doesNotExist = Message(obj.sender, "SERVER", doesNotExist)
+            # sendToClient(obj.sender, doesNotExist.encode())
+
+            sendToClient(obj.sender, "The user you are trying to contact does not exist.")
+
+        # If recipient is logged-out -> Alerts sender and and queues the message
+        elif not loginStatus[obj.recipient]:
+            # notlogin = obj.recipient + " is not logged in. But your message will be delivered"
+            # notlogin = Message(obj.sender, "SERVER", notlogin)
+            # sendToClient(obj.sender,notlogin.encode())
+
+            sendToClient(obj.sender, f"{obj.recipient} is not logged in. But your message will be delivered")
+
+            if obj.recipient not in queuedMessages.keys():
+                queuedMessages[obj.recipient] = []
+            queuedMessages[obj.recipient].append(obj)
+
+        # Else sends the message to recipient and delivery confirmation to sender
+        elif (clientID[obj.recipient] in clients):
+            # success = "Your message has successfully delivered."
+            # success = Message(obj.sender, "SERVER", success)
+            # sendToClient(obj.recipient, obj.encode())
+            # sendToClient(obj.sender, success.encode())
+            clientID[obj.recipient].send(obj.encode())
+            sendToClient(obj.sender, "Your message has successfully delivered.")
+    
+    # If the object is a Command() then we execute the command
+    if isinstance(obj, Command):
+
+        # If Command type is DA -> Delete Account
+        if obj.actionType == "DA":
+                
+            # User must be online execute this Command so no checks are needed
+
+            # Send confirmation to user
+            # success = "Account-Successfully-Deleted"
+            # success = Message(obj.username, "SERVER", success)
+            # sendToClient(obj.username, success.encode())
+            sendToClient(obj.username,"Account-Successfully-Deleted")
+
+
+            # Remove user from loginStatus and clientID list
+            del loginStatus[obj.username]
+            del clientID[obj.username]
+
+            # Only delete from queued messages if user had queued messages. 
+            if obj.username in queuedMessages.keys():
+                del queuedMessages[obj.username]
+
+        # If Command type is LA -> Lists all stored accounts with their login status
+        elif obj.actionType == "LA":
+
+            ## Generate account list
+            allAccounts = 'LA|'
+
+            # Append (active) or (inactive) to every username
+            for account in obj.data:
+
+                #  If username is active
+                if obj.data[account]:
+                    status = 'active'
+
+                # If username is not active
+                else:
+                    status = 'inactive'
+                
+                # Add account status to list with "|", as a divider
+                allAccounts += account + " ( " + status + " )" + "|"
+
+            # Remove the last "|" if accounts exists 
+            if len(allAccounts) > len("LA|"):
+                allAccounts = allAccounts[:-1]
+
+            # Sends list to client to display
+            # allAccounts = Message(obj.username, "SERVER", allAccounts)
+            # sendToClient(obj.username, allAccounts.encode())
+            sendToClient(obj.username, allAccounts)
+
+
+# Protocol unpack returns Message() and Command() from protocol buffers
+# Takes a client socket and encoded buffer data
+def protocol_unpack(client):
+    # Receives bits from client and converts to buffer(encoded)
+    data = receiveData(client)
+   
+    # Buffer data and splits on the colon 
+    dataSplit = data.split(":",3)
+
+    # Retrieves type of message/command from buffer data
+    type_ = dataSplit[0]
+
+    # If type is M -> create and return Message() object
+    if type_ == "M":
+
+        # Return Message()
+        return Message.createMessageFromBuffer(data)
+
+    # If type is LA -> create and return Command() object
+    elif type_ == "LA":
+        
+        # Get username from buffer
+        username = dataSplit[1]
+
+        # Return Command() with server login status information
+        return Command.createCommandFromBuffer(client, loginStatus ,username,type_)
+
+    # If type is DA -> create and return Command() object
+    elif type_ == "DA":
+
+        # Get username from buffer
+        username = dataSplit[1]
+
+        # Return Command() 
+        return Command.createCommandFromBuffer(client, None ,username,type_)
+
+
+# Function sends a message to all active accounts 
+def broadcast(message):
+    
+    # Loops through all active users
+    for client in clients:
+
+        # Finds username and creates a message object
+        index = clients.index(client)
+        username = usernames[index]
+        
+        # # Sends message to user
+        sendToClient(username,message)
+
+# Server thread to handle client <-> server communications
+def handle_client(client):
+
+    # Displays All accounts and statuses when the client connects
+
+    ## Generate account list
+    allAccounts = 'LA|'
+
+    for account in loginStatus:
+
+        # If username is active
+        if loginStatus[account]:
+            status = 'active'
+
+        # If username is inactive
+        else:
+            status = 'inactive'
+
+        # Add account status to list with "|", as a divider
+        allAccounts += account + " ( " + status + " )" + "|"
+
+    # Remove the last "|" if accounts exists 
+    if len(allAccounts) > len("LA|"):
+        allAccounts = allAccounts[:-1]
+    
+    # Sends List of all account statuses to client 
+    message = f"M:_:SERVER:{allAccounts}".encode('utf-8')
+    header = f"{len(message) :< {HEADER_LENGTH}}".encode('utf-8')
+    client.send(header+message)
+
+
+    ## Account Register/Login Loop
+
+    # Client starts unauthorized
+    client_auth = False
+
+    # While user is not authorized attempt to authenticate
+    while not client_auth:
+         
+        # Recieve client Command -- Either type CA (Create Account) or L(Login)
+        data = receiveData(client)
+
+        # Split buffer data and extract the username and type that were sent   
+        data = data.split(":",3)
+        type_ = data[0]
+        username = data[1]
+        
+
+        # If type is CA -> attempt to create an account with the username
+        if type_ == "CA":
+
+            # If username already exists -> Send client error
+            if username in loginStatus:
+                success = "Username-Already-Exists."
+                success = Message(username, "SERVER", success)
+                client.send(success.encode())
+
+            # If username doesn't exist -> Send client account creation confirmation
+            else:
+
+                # Adds user to the server and logs them in
+                usernames.append(username)
+                clients.append(client)
+                clientID[username] = client
+                loginStatus[username] = True
+
+                sendToClient(username,"Successful-Account-Creation.")
+
+                # Broadcasts new connection and authenticates connection
+                onConnection(username)
+                client_auth = True
+
+
+        # If type is L -> attempt to Login in with username
+        elif type_ == "L":
+
+            # If user does not exist -> send client login failure message
+            if username not in loginStatus:
+                failed = "Login-Failed"
+                failed = Message(username,"SERVER", failed)
+                client.send(failed.encode())
+
+            # If user is already active -> alert client!
+            elif loginStatus[username]:
+                active = "Account-Already-Active"
+                active = Message(username, "SERVER", active)
+                client.send(active.encode())
+
+            # If username exists and is not active -> log user in
+            else:
+                # Log user in
+                clients.append(client)
+                usernames.append(username)
+                clientID[username] = client
+                loginStatus[username] = True
+
+                # Send confirmation message
+                sendToClient(username,"Login-Successful.")
+
+                # Broadcast connection and deliver messages and authenticate client
+                onConnection(username)
+                client_auth = True
+
+    # Receives buffers from client and applies wire protocol
+    while True:
+        try:
+            
+
+            # Applies wire protocol to buffer -> returns a Message() or Command() objects
+            obj = protocol_unpack(client)
+
+            # Applies an action to the object
+            protocol_action(obj)
+
+
+        # This exception handles client crashes and logouts
+        except:
+
+            # Removes client from active user lists
+            index = clients.index(client)
+            clients.remove(client)
+
+            # Get clients username and then remove them from active user lists
+            username = usernames[index]
+            usernames.remove(username)
+
+            
+            # Only change loginStatus is user account isn't deleted
+            if username in loginStatus:
+                loginStatus[username] = False
+
+            # Broadcast to everyone that the client disconnected
+            broadcast(f'{username} has left the chat room!')
+            break
+
+
+# receive() listens for client connections and starts new threads when found
+def receive():
+
+    # When server starts
+    print('Server is running and listening ...')
+
+    while True:
+
+        # Accepts new client on client connection
+        client, address = server.accept()
+        
+        # Logs client connection information
+        print(f'connection is established with: {str(address)}')
+        print((f'connection is established with CLIENT :\n{client}'))
+
+        # Creates a new thread to handle client-server communication
+        thread = threading.Thread(target=handle_client, args=(client,))
+        thread.start()
+
+# onConnection() broadcasts a username and dequeues their messages
+def onConnection(username):
+
+    # User connection broadcast
+    message = f'{username} has connected to the chat room'
+    broadcast(message)
+
+    # Dequeing of stored user messages
+    if username in queuedMessages.keys():
+        for message in queuedMessages[username][:]:
+
+            # Finds socket associated with user
+            client = clientID[username]
+
+            # Sends encoded message to that user
+            client.send(message.encode())
+
+            queuedMessages[username].remove(message)
+
+if __name__ == "__main__":
+
+    time.sleep(3)
+
+    serverThriSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverThriSocket.bind((HOST, 12350))
+    serverThriSocket.connect((HOST, 12340))
+
+    time.sleep(3)
+    receive()
